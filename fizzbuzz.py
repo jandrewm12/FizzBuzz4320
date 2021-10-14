35e2fae5ae6a4d0dfdb9d6eac2c18276c77b0e11
jandrewm12 <60158099+jandrewm12@users.noreply.github.com>
2021-10-13 21:16:43 -0500

    Add files via upload
    Final Submission - ensure code is readable/understandable/clean. This version of the program adds comments so the reader will know what each line of code does and why it's there.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,25 +1,20 @@
 #FizzBuzz - INFOTC 4320
 
+# main function
 def main():
-
-    i = 1
-
+    # for loop that will loop through numbers 1 - 100
     for i in range(1, 101):
-
+        # if the number the loop is currently on is divisible by 3 and 5, it will print FizzBuzz
         if (i % 3 == 0) and (i % 5 == 0):
-
             print("FizzBuzz")
-
+        # if the number the loop is currently on is divisible by 3, it will print Fizz
         elif i % 3 == 0:
-
             print("Fizz")
-
+        # if the number the loop is currently on is divisible by 3, it will print Buzz
         elif i % 5 == 0:
-
             print("Buzz")
-
+        # if the number is divisible by neither 3 or 5, it will just print the number
         else:
-
             print(i)
-
+# call main function
 main()
